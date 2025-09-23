88c90cc94e802cec6aa1a76d753b474d569e8315
Juber_Mulla <juberjilanimulla@gmail.com>
2025-09-23 11:47:34 +0530

    second commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,59 @@
 import React from "react";
 import "../styles/Contact.css";
+import theme_pattern from "../assets/theme_pattern.svg";
+import mail_icon from "../assets/mail_icon.svg";
+import location_icon from "../assets/location_icon.svg";
+import call_icon from "../assets/call_icon.svg";
 
 const Contact = () => {
-  return <div className="contact"></div>;
+  return (
+    <div className="contact">
+      <div className="contact-title">
+        <h1>Get in touch</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="contact-section">
+        <div className="contact-left">
+          <h1>Let's talk</h1>
+          <p>
+            I'm currently available to take on new projects,so feel free to send
+            me a message about anything that you want me to work on. You can
+            contact anytime.
+          </p>
+          <div className="contact-details">
+            <div className="contact-detail">
+              <img src={mail_icon} alt="" />
+              <p>juber@firstclusive.com</p>
+            </div>
+            <div className="contact-detail">
+              <img src={call_icon} alt="" />
+              <p>+918600710244</p>
+            </div>
+            <div className="contact-detail">
+              <img src={location_icon} alt="" />
+              <p>Jawahar Nagar Kolhapur Maharashtra India</p>
+            </div>
+          </div>
+        </div>
+
+        <form className="contact-right">
+          <label htmlFor="">Your Name</label>
+          <input type="text" placeholder="Enter Your Name" name="name" />
+          <label htmlFor="">Your Email</label>
+          <input type="text" placeholder="Enter your email" name="email" />
+          <label htmlFor="">Write your message</label>
+          <textarea
+            name="message"
+            rows="8"
+            placeholder="Enter your message"
+          ></textarea>
+          <button type="submit" className="contact-submit">
+            Submit Now
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
