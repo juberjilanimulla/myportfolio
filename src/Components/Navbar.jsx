2ad22950579338843a2249af4620546bbe3d7ab8
Juber_Mulla <juberjilanimulla@gmail.com>
2025-09-24 12:14:08 +0530

    this final commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "../styles/Navbar.css";
 import logo from "../assets/logo.svg";
 import nav_underline from "../assets/nav_underline.svg";
@@ -8,12 +8,24 @@ import menu_close from "../assets/menu_close.svg";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
+  const menuRef = useRef();
+  const openMenu = () => {
+    menuRef.current.style.right = "0";
+  };
+  const closeMenu = () => {
+    menuRef.current.style.right = "-350px";
+  };
   return (
     <div className="navbar">
       <img src={logo} alt="" />
-      <img src={menu_open} alt="" />
-      <ul className="nav-menu">
-        <img src={menu_close} alt="" className="nav-mob-close" />
+      <img src={menu_open} onClick={openMenu} alt="" className="nav-mob-open" />
+      <ul ref={menuRef} className="nav-menu">
+        <img
+          src={menu_close}
+          onClick={closeMenu}
+          alt=""
+          className="nav-mob-close"
+        />
         <li>
           <AnchorLink className="anchor-link" href="#home">
             <p onClick={() => setMenu("home")}>Home</p>
